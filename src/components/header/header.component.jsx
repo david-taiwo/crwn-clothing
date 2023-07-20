f86bb2ecb7a110c920e559b07613e4c20743763b
Your Namedavid-taiwo <davidtaiwo003@gmail.com>
2023-07-20 18:23:39 +0100

    added redux, and implemented userReducer and userActions to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom";
+
+import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
 
 import { ReactComponent as Logo } from "../../assets/4.1 crown.svg";
 import "./header.styles.css";
+
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -30,4 +33,9 @@ const Header = ({ currentUser }) => (
     </div>
   </div>
 );
-export default Header;
+
+const mapStateToProps =state=> ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 import './index.css';
 import App from './App';
@@ -8,11 +10,13 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>  
 );
 
 reportWebVitals();
